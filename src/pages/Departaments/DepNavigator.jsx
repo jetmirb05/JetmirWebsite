b31b8f8f8a2049a6b8966ee2572f033a2a19dda0
JetmirThor <jetmir.bela@thorindustriesmk.com>
2023-11-23 17:46:07 +0100

    fix: changes

--- src/pages/Departaments/DepNavigator.jsx
+++ src/pages/Departaments/DepNavigator.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
     return (
     <>
         <motion.div variants={ContainerAnimation} initial="hidden" animate="show" className="bg-thorwhite sm:mt-[0px] mt-[20px] sm:w-[50%] lg:w-[240px]  md:w-[300px] text-thorblack ml-[2vw] sm:ml-[0px]  h-[230px] border-2 p-[20px] flex flex-col justify-around ">
-          <motion.h1 variants={itemAnimation} className="font-bold text-2xl ">{t('depart')}</motion.h1>
+          <motion.h1 variants={itemAnimation} className="font-bold text-2xl ">{t('product')}</motion.h1>
             <motion.h5 variants={itemAnimation} className={isMining.Name == "min" ? 'text-thormining' : 'text-black hover:text-thormining'}><Link to="/MiningDep">{t('mac')}</Link></motion.h5>
             <motion.h5 variants={itemAnimation} className={isMining.Name == "cnst" ? 'text-thorconstruction ' : 'text-black hover:text-thorconstruction'}><Link to="/ConstructionDep">{t('iphone')}</Link></motion.h5>
             <motion.h5 variants={itemAnimation} className={isMining.Name == "rnw" ? 'text-thorenergy' : 'text-black hover:text-thorenergy'}><Link to="/RenewableEnergyDep">{t('ipad')}</Link></motion.h5>
